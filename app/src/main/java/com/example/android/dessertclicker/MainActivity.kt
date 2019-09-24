7a15042fab3e84cce6d23f3640abefb7ceed6a6f
Zerocool <Phichaya6310@gmail.com>
2019-09-24 22:55:26 +0700

    Setup Timber

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -78,13 +79,13 @@ class MainActivity : AppCompatActivity() {
 
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
-        Log.i("MainActivity", "onCreate Called")
+        Timber.i("onCreate called")
     }
 
     override fun onStart() {
         super.onStart()
+        Timber.i("onStart Called")
 
-        Log.i("MainActivity", "onStart Called")
     }
 
     /**
